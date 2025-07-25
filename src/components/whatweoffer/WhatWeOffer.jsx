54671d97fcddbbb4784d99690dae58bc19710b0d
SulhanOzgur <sulhan_ozgur@hotmail.com>
2025-07-25 21:04:26 +0300

    WhatWeOffer completed
    Home Page WhatWeOffer completed.

--- src/components/whatweoffer/WhatWeOffer.jsx
+++ src/components/whatweoffer/WhatWeOffer.jsx
@@ -1,5 +1,30 @@
-import { BookOpen, BarChart, TrendingUp } from 'lucide-react';
+import { services } from '../../data/WhatWeOffer';
 
+//kodları incele
 export default function WhatWeOffer() {
-  return;
+  return (
+    <section className="w-[1050px] h-[616px] mx-auto my-5">
+      {/* Title */}
+      <div className="w-[692px] h-[102px] mx-auto text-center">
+        <h2 className="text-xl text-brand-lightgray">Featured Products</h2>
+        <h3 className="text-2xl font-bold text-brand-DEFAULT my-2">
+          THE BEST SERVICES
+        </h3>
+        <p className="text-sm text-brand-lightgray">
+          Problems trying to resolve the conflict between
+        </p>
+      </div>
+
+      {/* Bottom */}
+      <div className="w-[1045px] h-[274px] mx-auto text-center flex justify-between mt-[80px]">
+        {services.map(({ id, icon: Icon, title, description }) => (
+          <div key={id} className="w-full md:w-1/3 space-y-4 px-4">
+            <img src={Icon} alt={title} className="w-18 h-18 mx-auto" />
+            <h3 className="text-2xl font-bold text-brand-DEFAULT">{title}</h3>
+            <p className="text-sm text-brand-lightgray">{description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
---
+++ src/data/WhatWeOffer.js
@@ -0,0 +1,25 @@
+import image1 from '../assets/logos/bx_bxs-book-reader.png';
+import image2 from '../assets/logos/carbon_book.png';
+import image3 from '../assets/logos/uil_arrow-growth.png';
+
+export const services = [
+  {
+    id: 1,
+    icon: image1,
+    title: 'Easy Wins',
+    description: 'Get your best looking smile now!',
+  },
+  {
+    id: 2,
+    icon: image2,
+    title: 'Concrete',
+    description:
+      'Defalcate is most focused in helping you discover your most beautiful smile',
+  },
+  {
+    id: 3,
+    icon: image3,
+    title: 'Hack Growth',
+    description: 'Overcame any hurdle or any other problem.',
+  },
+];
